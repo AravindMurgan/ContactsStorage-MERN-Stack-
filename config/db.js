76ba8455b96304ffd04f5f0b8c7cff2cb6a45ba2
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-02-13 16:34:48 +0530

    express&mongoDB setup done

--- config/db.js
+++ config/db.js
@@ -3,16 +3,25 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 
-const connectDB = ()=>{
-    mongoose.connect(db,{
-        useNewUrlParser:true,
-        useCreateIndex:true,
-        useFindAndModify:false,
-        
-    })
-    .then(()=>console.log('MongoDB is Connected'))
-    .catch((err)=>console.log(err));
-    process.exit(1);
+const connectDB = async()=>{
+
+    try {
+        mongoose.connect(db,{
+            useNewUrlParser:true,
+            useCreateIndex:true,
+            useFindAndModify:false,
+            useUnifiedTopology: true
+            
+        })
+        console.log('MongoDB Connected...');
+    } 
+    catch (err) {
+       console.log(err.message);
+        process.exit(1);
+    }
+
+
+   
 }
 
 
