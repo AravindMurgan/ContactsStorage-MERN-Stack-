4af09d67422000b234e3f78aaa9cacc49eb80858
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-02-14 17:17:26 +0530

    user-auth

--- routes/users.js
+++ routes/users.js
@@ -12,16 +12,40 @@ router.post('/',[
     check('name','name is required').not().isEmpty(),
     check('email','email is required').isEmail(),
     check('password','password should be atleast min 6 characters').isLength({min:6}),
-],(req,res)=>{
+],async (req,res)=>{
     const errors = validationResult(req);
 
     if(!errors.isEmpty()){
         
         return res.status(400).json({errors: errors.array()});
-    }else{
-        res.send('passed');
     }
 
+    const {name,email,passwowrd}= req;
+    const bcrypt = require('bcryptjs');
+
+    try {
+        let user = await User.findOne({email});
+
+        if(user){
+            return res.status(400).json({msg:'User already exists'})
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(passwowrd,salt);
+
+        await user.save();
+
+        res.send('User Saved');
+    } catch (error) {
+        
+    }
     
 })
 
