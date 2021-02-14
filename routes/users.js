24b806f42cab261bfdab915833031ff67bf983a7
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-02-14 14:31:55 +0530

    userModel&validation

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // @desc   Register a user//
 // &access Public
 router.post('/',(req,res)=>{
-    res.send('Register a user');
+    res.send(req.body);
 })
 
 module.exports = router;
