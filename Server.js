836bc52c820f266c3ff8325f0a7bd0cf6ec354db
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-02-14 13:17:36 +0530

    express-practice

--- Server.js
+++ Server.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const connectDB = require('./config/db');
+// const connectDB = require('./config/db');
 
 const app = express();
 
-//Connect DB//
-connectDB();
+
+const { sup, how } = require("./middle");
+
+app.use(sup);
+
+// //Connect DB//
+// connectDB();
 
 app.get('/',(req,res)=>
     res.json({
@@ -14,11 +19,36 @@ app.get('/',(req,res)=>
     })
 )
 
-// /// Define Routes///
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/contacts',require('./routes/contacts'))
+// // /// Define Routes///
+// app.use('/api/users', require('./routes/users'));
+// app.use('/api/auth',require('./routes/auth'));
+// app.use('/api/contacts',require('./routes/contacts'))
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
+// app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
+
+
+// const http = require('http');
+// console.log(http.STATUS_CODES);
+
+app.get("/",(req,res)=>{
+    res.send('Hello Aravind here')
+})
+
+// app.get("/old",(req,res)=>{
+//     res.redirect(301,"/new/mynew")
+// })
+
+// app.get("/new/mynew",(req,res)=>{
+//     res.send('This is a new URL')
+// })
+
+/// Middleware - How Express handles a sequence of functions like  GET POST PUT DELETE
+app.listen(5000, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("listening on port 5000");
+  });
---
+++ app.js
@@ -0,0 +1,22 @@
+//Understanding Middleware
+"use strict";
+const express = require("express");
+const app = express();
+// const cors = require("cors");
+
+const { sup, how } = require("./middle");
+
+app.use(sup);
+// app.use(cors());
+
+app.get("/", how, sup, (req, res) => {
+  res.send({ data: "hi" });
+});
+
+app.listen(8000, err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("listening on port 3000");
+});
