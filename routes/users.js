269533f27b48c2410cd22977b67450fd27ca63f3
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-02-14 16:33:48 +0530

    express-validation

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
 const router = express.Router();
 
 const User = require('../model/User');
@@ -7,9 +8,16 @@ const User = require('../model/User');
 // $route  POST(api/users)//
 // @desc   Register a user//
 // &access Public
-router.post('/',(req,res)=>{
+router.post('/',[
+    check('name','name is required').not().isEmpty(),
+    check('email','email is required').isEmail(),
+    check('password','password should be atleast min 6 characters').isLength({min:6})
+],(req,res)=>{
+    const errors = validationResult(req);
 
-    res.send(req.body);
+    if(!errors.isEmpty()){
+        
+    }
 })
 
 module.exports = router;
