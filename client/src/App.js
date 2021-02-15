083006a2fa0de098876d3fc61e497955ec299a0a
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-02-15 23:43:24 +0530

    font-awesome

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Adpp">
       <h1>Hello React Baby</h1>
+      <p>Virtual DOM</p>
+      <p>Virtual DOM</p>
+      <p>Virtual DOM</p>
     </div>
   );
 }
