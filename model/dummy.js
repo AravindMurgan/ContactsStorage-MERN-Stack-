f08b77d9841a25351ca7b0c062d2e0965c354011
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-02-15 16:03:46 +0530

    contact-allAPIrequestadded

--- model/dummy.js
+++ model/dummy.js
@@ -1,29 +1,22 @@
-const mongoose = require('mongoose');
+// @route     DELETE api/contacts/:id
+// @desc      Delete contact
+// @access    Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
 
-const ContactSchema = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'users'
-  },
-  name:{
-    type:String,
-    required:true
-  },
-  email:{
-    type:String,
-    required:true
-  },
-  phone:{
-    type:String
-  },
-  type:{
-    type:String,
-    default:'personal'
-  },
-  date:{
-    type:Date,
-    default:Date.now
+    if (!contact) return res.status(404).json({msg: 'Contact not found'});
+
+    // Make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({msg: 'Not authorized'});
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({msg: 'Contact removed'});
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
 });
-
-module.exports = mongoose.model('contact',ContactSchema);
