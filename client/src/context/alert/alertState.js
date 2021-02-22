e59e68212e9ff8e9c759c8b56b57626ec4002570
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-02-22 16:14:38 +0530

    alertReducer functions

---
+++ client/src/context/alert/alertState.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import AlertContext from './alertContext';
+import AlertReducer from './alertReducer';
+import ContactItem from '../../component/contacts/ContactItem';
+
+import { SET_ALERT, REMOVE_ALERT } from '../type';
+
+const AlertState = (props) => {
+	const initialState = [];
+
+	const [state, dispatch] = useReducer(AlertReducer, initialState);
+
+	const setAlert = (msg, type, timeout = 5000) => {
+		contact.id = uuidv4();
+
+		dispatch({
+			type: SET_ALERT,
+			payload: { msg, type, id },
+		});
+
+		setTimeout(() =>
+			dispatch({ type: REMOVE_ALERT, payload:id}, timeout)
+		);
+	};
+
+	return (
+		<AlertContext.Provider value={{}}>{props.children}</AlertContext.Provider>
+	);
+};
+
+export default AlertState;
