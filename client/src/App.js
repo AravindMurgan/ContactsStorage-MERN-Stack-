69aeb1e59eec993ebc7d17273ed95727bd25dde4
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-02-24 00:21:19 +0530

    Private-Route done

--- client/src/App.js
+++ client/src/App.js
@@ -8,38 +8,36 @@ import Alert from './component/layout/Alert';
 import Home from './component/pages/Home';
 import SetAuthToken from './utils/setAuthToken';
 import './App.css';
-
+import PrivateRoute from './component/routing/PrivateRoute';
 
 import ContactState from './context/contact/contactState';
 import AuthState from './context/auth/authState';
 import AlertState from './context/alert/AlertState';
 
-
-if(localStorage.token){
-	SetAuthToken(localStorage.token)
+if (localStorage.token) {
+	SetAuthToken(localStorage.token);
 }
-
 
 function App() {
 	return (
 		<AuthState>
 			<ContactState>
-			<AlertState>
-				<Router>
-					<Fragment>
-						<Navbar />
-						<div className='container'>
-							<Alert />
-							<Switch>
-								<Route exact path='/' component={Home} />
-								<Route exact path='/about' component={About} />
-								<Route exact path='/register' component={Register} />
-								<Route exact path='/login' component={Login} />
-							</Switch>
-						</div>
-					</Fragment>
-				</Router>
-			</AlertState>
+				<AlertState>
+					<Router>
+						<Fragment>
+							<Navbar />
+							<div className='container'>
+								<Alert />
+								<Switch>
+									<PrivateRoute exact path='/' component={Home} />
+									<Route exact path='/about' component={About} />
+									<Route exact path='/register' component={Register} />
+									<Route exact path='/login' component={Login} />
+								</Switch>
+							</div>
+						</Fragment>
+					</Router>
+				</AlertState>
 			</ContactState>
 		</AuthState>
 	);
